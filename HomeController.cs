17212be68e3da90d38ef6d5ae3b9b87ee930ca53
callmelele <140650638+callmelele@users.noreply.github.com>
2024-11-28 15:15:53 +0100

    Update HomeController.cs

--- HomeController.cs
+++ HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 using System.Diagnostics;
 using WebApplication1.Models;
 
@@ -6,19 +8,83 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IWebHostEnvironment env)
         {
-            _logger = logger;
+            environment = env;
         }
 
-        public IActionResult Index()
+        private string GetConnectionString(string databaseFileName)
+        {
+            return environment.IsDevelopment()
+                ? @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={environment.ContentRootPath}{databaseFileName};Integrated Security=True;Connect Timeout=15"
+                : @$"Data Source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename={environment.ContentRootPath}{databaseFileName};User Instance=true;Connect Timeout=15";
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            userModel userModel = new userModel();
+            DataTable LocalTbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(GetConnectionString("myStore.mdf")))
+            {
+                sqlCon.Open();
+                string query = "SELECT * FROM tblUser WHERE UID = @Id";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("Id", id);
+                sqlDa.Fill(LocalTbl);
+            }
+            if (LocalTbl.Rows.Count == 1)
+            {
+                userModel.UID = Convert.ToInt32(LocalTbl.Rows[0][0].ToString());
+                userModel.Username = LocalTbl.Rows[0][3].ToString();
+                userModel.Password = LocalTbl.Rows[0][4].ToString();
+                userModel.Förnamn = LocalTbl.Rows[0][1].ToString();
+                userModel.Efternamn = LocalTbl.Rows[0][4].ToString();
+                userModel.Picture = LocalTbl.Rows[0][5].ToString();
+                return View(userModel);
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+        }
+
+
+        public ActionResult create()
         {
             return View();
         }
+        public ActionResult Index()
+        {
+            DataTable localTbl = new DataTable();
+            using (SqlConnection connection = new SqlConnection(GetConnectionString("WebApplication1"))) 
+            {
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM tblUser", connection);
+                connection.Open();
+                sqlDa.Fill(localTbl);
+            }
+            return View(localTbl);
+        }
 
-        public IActionResult Privacy()
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(GetConnectionString("myStore.mdf")))
+            {
+                sqlConn.Open();
+
+                string query = "DELETE FROM tblUser WHERE UID = @Id";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlConn);
+                sqlCmd.Parameters.AddWithValue("@Id", id);
+                sqlCmd.ExecuteReader();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+    public IActionResult Privacy()
         {
             return View();
         }
